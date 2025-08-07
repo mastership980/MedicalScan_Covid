1c78ffce3c1e656f77afea82f5ef789bdb1c255e
Laci <r.lacus25@gmail.com>
2025-08-08 00:50:51 +0200

    Commentek írva, kisseb margin hiba javítva

--- js/validate.js
+++ js/validate.js
@@ -1,10 +1,13 @@
 (() => {
   'use strict';
 
+  // Minden form kiválasztása, ami validációt igényel
   const forms = document.querySelectorAll('.needs-validation');
 
+  // Minden formhoz eseményfigyelő
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
+      // Hozzászólás mező hossza minimum 10 karakter  
       const desc = form.querySelector('#desc');
       if (desc.value.trim().length < 10) {
         desc.setCustomValidity('too short');
@@ -12,11 +15,13 @@
         desc.setCustomValidity('');
       }
 
+      // Ha nem valid, elküldést megakadályozza
       if (form.checkValidity() == false) {
         event.preventDefault();
         event.stopPropagation();
       }
 
+      // Bootstrap validációs stílus hozzáadása
       form.classList.add('was-validated');
     }, false);
   });
